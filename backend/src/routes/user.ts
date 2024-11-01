18111a0fddc3349f95c260e97ca01f5ee7b159bb
rishav_learnerml <rishavbio1@gmail.com>
2024-11-01 21:13:39 +0530

    feed api built

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authChecker from "../middlewares/authChecker";
 import RequestWithCookieType from "../types/interfaces";
 import { ConnectionRequest } from "../models/connectionRequest";
+import { User } from "../models/user";
 
 const userRouter = Router();
 
@@ -60,6 +61,37 @@ userRouter.get(
       });
 
       res.json({ message: "Success", data });
+    } catch (error) {
+      res.status(400).json({ message: "Something went wrong!", error });
+    }
+  }
+);
+
+userRouter.get(
+  "/feed",
+  authChecker,
+  async (req: RequestWithCookieType, res) => {
+    try {
+      const loggedInUser = req.user;
+      const connections = await ConnectionRequest.find({
+        $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+      }).select("fromUserId toUserId");
+
+      const hideFromFeed = new Set();
+
+      connections.forEach((req) => {
+        hideFromFeed.add(req.fromUserId.toString());
+        hideFromFeed.add(req.toUserId.toString());
+      });
+
+      const users = await User.find({
+        $and: [
+          { _id: { $nin: Array.from(hideFromFeed) } },
+          { _id: { $ne: loggedInUser._id } },
+        ],
+      }).select(USER_SAFE_DATA);
+
+      res.json({ message: "Success", users });
     } catch (error) {
       res.status(400).json({ message: "Something went wrong!", error });
     }
